615c6c47bdde068274369118ccbac77cdc76323d
Prem N Chalase <premchalase3@gmail.com>
2026-05-29 01:07:09 +0530

    changed theme color

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -2,7 +2,7 @@
 
 export const lightTheme = {
 
-    body:"#FCF6F4",
+    body:"rgb(254, 240, 183)",
     text:"#000000",
     fontFamily:"'Source Sans Pro', sans-serif",
     bodyRgba : "252, 246, 244",
@@ -12,7 +12,7 @@ export const lightTheme = {
 export const DarkTheme = {
 
     body:"#000000",
-    text:"#FCF6F4",
+    text:"rgb(254, 240, 183)",
     fontFamily:"'Source Sans Pro', sans-serif",
     textRgba : "252, 246, 244",
     bodyRgba:"0,0,0",
